d899ba351715b1e5208ea368e62d781a6b051007
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-11-25 13:10:38 -0800

    Updating with accordance with Echo.h

--- Echo.cpp
+++ Echo.cpp
@@ -1,1 +1,31 @@
+/*
+ * Authors: Amaan Sidhu, Andre Lou, Thomas Caneday
+ * Date: Nov 25, 2021
+ * Assignment: Semester Project
+*/
 
+#include <string>
+#include <iostream>
+#include "Echo.h"
+#include "Wav.h"
+#include "Processor.h"
+Echo::Echo()
+{
+
+}
+Echo::Echo(int delay) : delay(delay)
+{
+	
+}
+Echo::~Echo()
+{
+	
+}
+void Echo::setDelay(int newDelay)
+{
+	delay = newDelay;
+}
+int Echo::getDelay()
+{
+	return delay;
+}
