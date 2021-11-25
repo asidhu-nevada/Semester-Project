650347dd1bea5667b2630958a008bd03295a4ca1
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-11-25 13:08:43 -0800

    Update Echo.h

--- Echo.h
+++ Echo.h
@@ -1,1 +1,14 @@
+#pragma once
 
+#include "Processor.h"
+#include <cstdint>
+#include <cmath>
+
+class Echo: public Processor {	   
+public:
+	int delay;
+	Echo();
+	int getDelay();
+	virtual ~Echo();
+	void setDelay(int newDelay);
+};
